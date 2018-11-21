f8f901059bd79a8bb44f6599cf320d56f6be8cb5
jacky.leung <jacky.leung@oocl.com>
2018-11-21 15:05:27 +0800

    [LEUNGJA] updated: map to even, map to four multiples add one

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,16 +1,20 @@
 'use strict';
-function times2(element) {
-  return element * 2;
+function map_to_even(collection) {
+  return collection.map(element => element * 2);
 }
 
-function map(collection, fn) {
-  for (let index = 0; index < collection.length; index++) {
-    collection[index] = fn(collection[index]);
-  }
-  return collection;
-}
+// function times2(element) {
+//   return element * 2;
+// }
 
-function map_to_even(collection){
-  return map(collection, times2);
-}
+// function map(collection, fn) {
+//   for (let index = 0; index < collection.length; index++) {
+//     collection[index] = fn(collection[index]);
+//   }
+//   return collection;
+// }
+
+// function map_to_even(collection){
+//   return map(collection, times2);
+// }
 module.exports = map_to_even;
--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,17 +1,23 @@
 'use strict';
-function times4Add1(element) {
-  return element * 4 + 1;
-}
 
-function map(collection, fn) {
-  for (let index = 0; index < collection.length; index++) {
-    collection[index] = fn(collection[index]);
-  }
-  return collection;
+var map_to_four_multiples_add_one = function(collection) {
+  return collection.map(element => element * 4 + 1);
 }
+ 
 
-var map_to_four_multiples_add_one = function(collection){
-  return map(collection, times4Add1);
-};
+// function times4Add1(element) {
+//   return element * 4 + 1;
+// }
+
+// function map(collection, fn) {
+//   for (let index = 0; index < collection.length; index++) {
+//     collection[index] = fn(collection[index]);
+//   }
+//   return collection;
+// }
+
+// var map_to_four_multiples_add_one = function(collection){
+//   return map(collection, times4Add1);
+// };
 
 module.exports = map_to_four_multiples_add_one;
