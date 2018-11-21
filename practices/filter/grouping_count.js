e448498bf14e44346bfb9edcd17167960295c056
jacky.leung <jacky.leung@oocl.com>
2018-11-21 15:00:35 +0800

    [LEUNGJA] updated: grouping count

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,17 +1,30 @@
 'use strict';
 
+// function grouping_count(collection) {
+
+//   let count = {};
+
+//   //implement here
+//   collection.forEach(element => {
+//     if (count[element] != null) {
+//       count[element] += 1;
+//     } else {
+//       count[element] = 1;
+//     }
+//   });
+//   return count;
+// }
+
 function grouping_count(collection) {
-
   let count = {};
-
-  //implement here
-  collection.forEach(element => {
+  collection.filter(element => {
     if (count[element] != null) {
       count[element] += 1;
     } else {
       count[element] = 1;
     }
   });
+
   return count;
 }
 
